6f2e38e6fc8431f08edd5f2886af2ab5b851caa2
Pugach Alexander <fotone97@yandex.ru>
2021-03-17 23:47:21 +0300

    Добавлен проект для тестирования COM порта

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialPort = new SerialPort();
+            serialPort.PortName = "COM3";
+            serialPort.BaudRate = 57600;
+
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            serialPort.Open();
+            while (true)
+            {
+                try
+                {
+                    string message = serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
